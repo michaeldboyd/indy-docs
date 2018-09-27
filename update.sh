7118df9f9d2968b8539beb2000afe89d4f48074d
Michael Joseph McKean <mjmckean11@gmail.com>
2018-09-27 15:47:12 -0600

    minor updates to update.sh
    Signed-off-by: Michael Joseph McKean <mjmckean11@gmail.com>

--- update.sh
+++ update.sh
@@ -1,20 +1,17 @@
 #!/bin/bash
 
 # Declare Variables
-
 # API call for most repos does not pull up documentation, should adjust probably
 repo="indy-sdk"
 repoVersion="v0.0.0"
 repoFilePath=""
 
 # Get Versions
-
 repoVersion=$( curl --silent "https://api.github.com/repos/hyperledger/$repo/releases/latest" | grep '"tag_name":' | sed -E 's/.*"([^"]+)".*/\1/' )
 echo ">>>>>>>>>>Latest version: $repoVersion"
 repoFilePath="docs/source/$repo/$repoVersion"
 
 # Add documentation if version has not been pulled
-
 if [ ! -d $repoFilePath ]; then
 
     # Create Version Folders
@@ -24,8 +21,8 @@ if [ ! -d $repoFilePath ]; then
     # Pull Repositories
     git clone https://github.com/hyperledger/$repo.git --branch $repoVersion
 
-    # Create Cargo Documentation
-    # cargo $repo/doc --no-deps
+    # Create Cargo Documentation for indy-sdk--needs to build too (is that worth it?)
+    # cargo doc --no-deps 
 
     # Move Documentation
     mv indy-sdk/doc $repoFilePath
